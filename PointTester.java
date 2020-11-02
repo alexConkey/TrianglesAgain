875cc1866405eae3ff8f7b3ab98c54d5b5d08889
alexConkey <aconkey20@stuy.edu>
2020-11-02 09:11:56 -0500

    distanceTo and equals completed

---
+++ PointTester.java
@@ -0,0 +1,7 @@
+public class PointTester {
+  public static void main(String[] args) {
+    Point myPoint = new Point(1, 1);
+    Point myPoint2 = new Point(1, 3);
+    System.out.println(myPoint.equals(myPoint2));
+  }
+}
